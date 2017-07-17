bbacacc33764a47fbf343571e7856a4891911d1c
Harrison McCord <harrison.mccord@gmail.com>
2017-07-17 12:14:16 -0400

    added api keys

--- keys.js
+++ keys.js
@@ -1,6 +1,9 @@
+console.log("Keys is Loaded");
+
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'iRPI0ZG4SVYlLzLlbyI2dscof',
+  consumer_secret: 'h9hIoVm7nz965RwvkfEmBv7BONeXLQ2pECi0avmYu2xTCwjOfc',
+  access_token_key: '885626302509846533-PLwV9C1mAdqo4yeYtpMARJJbGmRYW6g',
+  access_token_secret: '0FAMBL96IbYNIMx6DZURoPktz9eeqQdkWEcb5vab7hLX9',
 }
+
